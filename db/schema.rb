2032b7c18819ce9e47d1433a1484caa83d34c94d
Clemence Marotte <clemence.marotte@gmail.com>
2025-03-29 15:07:24 +0100

    add style show compo

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_29_065154) do
     t.index ["user_id"], name: "index_compositions_piano_on_user_id"
   end
 
+  create_table "daily_challenges", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "difficulty"
+    t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "notes", force: :cascade do |t|
     t.string "key"
     t.integer "tempo"
@@ -97,6 +106,16 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_29_065154) do
     t.index ["user_id"], name: "index_user_answers_on_user_id"
   end
 
+  create_table "user_challenges", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "challenge_id", null: false
+    t.boolean "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["challenge_id"], name: "index_user_challenges_on_challenge_id"
+    t.index ["user_id"], name: "index_user_challenges_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -119,4 +138,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_29_065154) do
   add_foreign_key "user_answers", "preferences"
   add_foreign_key "user_answers", "questions"
   add_foreign_key "user_answers", "users"
+  add_foreign_key "user_challenges", "challenges"
+  add_foreign_key "user_challenges", "users"
 end
