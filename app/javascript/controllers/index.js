61481c55e5e7963a5ac8b9840c85a5b10074c880
Clémence M. <clemence.marotte@gmail.com>
2025-04-03 21:32:02 +0200

    Revert "form modified"

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,5 +2,3 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-import MoodController from "./mood_controller";
-application.register("mood", MoodController);
