6929bc95f395cec59bc833d1c57fba88756b52ce
Ludovico CAMPONOVO <ludovico.camponovo@hotmail.fr>
2025-04-04 19:20:01 +0200

    petites retouches/challenges

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -2,14 +2,7 @@ class ChallengesController < ApplicationController
   require 'time'
 
   def index
-    @challenge = Challenge.where(active: true).order("RANDOM()").first
-
-    if session[:last_challenge_time].nil? || Time.now - Time.parse(session[:last_challenge_time]) > 300
-      session[:last_challenge_time] = Time.now.to_s # Stocke sous forme de String
-      session[:current_challenge_id] = Challenge.order("RANDOM()").first.id
-    end
-
-    @current_challenge = Challenge.find(session[:current_challenge_id])
+    @current_challenge = Challenge.where(active: true).order("RANDOM()").first
   end
 
   def show
